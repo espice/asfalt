ea14dbc065afb1d0366052256518aac4e44e0537
Arnav Bansal <arnavbansal101@gmail.com>
2023-08-28 00:28:33 +0530

    add agents query

--- apps/web/app/(pages)/(dadacon)/dada/con/(admin)/agents/page.tsx
+++ apps/web/app/(pages)/(dadacon)/dada/con/(admin)/agents/page.tsx
@@ -1,3 +1,24 @@
-export default function AgentsPage() {
-  return <div>Agents</div>;
+import { gqlClient } from "@/utils/gql";
+
+async function getAgents() {
+  const agents = await gqlClient().query({
+    agents: {
+      username: true,
+      id: true,
+      isAdmin: true,
+      sessions: {
+        id: true,
+        ip: true,
+        device: true,
+      },
+    },
+  });
+
+  return agents.agents;
+}
+
+export default async function AgentsPage() {
+  const agents = await getAgents();
+
+  return <div>{agents.length}</div>;
 }
